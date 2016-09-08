83c23bb357c28cbf65e3bf94c9e63778463a8e81
elseyu <elseying@gmail.com>
2016-09-08 15:17:40 +0800

    去掉了MatrixView

--- src/com/sunniwell/player/youtube/widget/matrix/OnItemSelectedListener.java
+++
@@ -1,8 +0,0 @@
-package com.sunniwell.player.youtube.widget.matrix;
-
-/**
- * Created by Administrator on 2016/8/30.
- */
-public interface OnItemSelectedListener {
-    void onItemSelected(int index);
-}
